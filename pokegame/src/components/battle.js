51c1f299b6af2edf72092f643008c12bf3ecebea
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2023-05-09 19:59:00 +0200

    figured out how css works with components
    and added some styling

--- pokegame/src/components/battle.js
+++ pokegame/src/components/battle.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from './styles/battlestyle.module.css'
 
 function BattleMenu(props){
 
@@ -6,17 +7,17 @@ function BattleMenu(props){
         props.setBattleState(false)
     }
     return (
-        <div>
+        <div className={styles.background}>
     
             <button onClick={() => GoBack()}>Go Back</button>
             <h1>This is the battle menu</h1>
             {
                 props.enemyPokemonData ? (
             <div>
-                <h2>{props.enemyPokemonData.name}</h2>
-                <img src={props.enemyPokemonData.sprites.front_default} text={props.enemyPokemonData.name}></img>
-                <h2>{props.userPokemonData.name}</h2>
-                <img src={props.userPokemonData.sprites.back_default} text={props.userPokemonData.name}></img>
+                <h2 className={styles.enemyName}>{props.enemyPokemonData.name}</h2>
+                <img src={props.enemyPokemonData.sprites.front_default} text={props.enemyPokemonData.name} className={styles.enemy}></img>
+                <h2 className={styles.playerName}>{props.userPokemonData.name}</h2>
+                <img src={props.userPokemonData.sprites.back_default} text={props.userPokemonData.name} className={styles.player}></img>
             </div>) : (
                 <h2>Loading players</h2>
             )
