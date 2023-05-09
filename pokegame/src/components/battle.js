c0afeb8f2fd8927f9a3f8e7e265cd3e2173b8f36
L.Radi <radi.levente@gmail.com>
2023-05-09 22:53:53 +0200

    add fight logics, declare winner, update pokemons HP

--- pokegame/src/components/battle.js
+++ pokegame/src/components/battle.js
@@ -1,30 +1,92 @@
-import React from "react";
-import styles from './styles/battlestyle.module.css'
+import React, { useState, useEffect } from "react";
+import styles from "./styles/battlestyle.module.css";
+import fight from "../fight";
 
-function BattleMenu(props){
+function BattleMenu(props) {
+  //console.log(fight);
 
-    function GoBack(){
-        props.setBattleState(false)
+  function GoBack() {
+    props.setBattleState(false);
+  }
+
+  const [attackerHP, setAttackerHP] = useState(
+    props.userPokemonData?.stats?.[0]?.base_stat || 0
+  );
+  const [defenderHP, setDefenderHP] = useState(
+    props.enemyPokemonData?.stats?.[0]?.base_stat || 0
+  );
+
+  useEffect(() => {
+    if (props.userPokemonData) {
+      setAttackerHP(props.userPokemonData.stats[0].base_stat);
     }
-    return (
-        <div className={styles.background}>
-    
-            <button onClick={() => GoBack()}>Go Back</button>
-            <h1>This is the battle menu</h1>
-            {
-                props.enemyPokemonData ? (
-            <div>
-                <h2 className={styles.enemyName}>{props.enemyPokemonData.name}</h2>
-                <img src={props.enemyPokemonData.sprites.front_default} text={props.enemyPokemonData.name} className={styles.enemy}></img>
-                <h2 className={styles.playerName}>{props.userPokemonData.name}</h2>
-                <img src={props.userPokemonData.sprites.back_default} text={props.userPokemonData.name} className={styles.player}></img>
-            </div>) : (
-                <h2>Loading players</h2>
-            )
-            }
+    if (props.enemyPokemonData) {
+      setDefenderHP(props.enemyPokemonData.stats[0].base_stat);
+    }
+  }, [props.userPokemonData, props.enemyPokemonData]);
+
+  return (
+    <div className={styles.background}>
+      <button onClick={() => GoBack()}>Go Back</button>
+      <h1>This is the battle menu</h1>
+      {props.userPokemonData && props.enemyPokemonData ? (
+        <div>
+          <h2 className={styles.enemyName}>{props.enemyPokemonData.name}</h2>
+          <img
+            src={props.enemyPokemonData.sprites.front_default}
+            text={props.enemyPokemonData.name}
+            className={styles.enemy}
+          ></img>
+          <h2 className={styles.playerName}>{props.userPokemonData.name}</h2>
+          <img
+            src={props.userPokemonData.sprites.back_default}
+            text={props.userPokemonData.name}
+            className={styles.player}
+          ></img>
+
+          <>
+            <h2>
+              {props.userPokemonData.name}'s HP: {attackerHP}
+            </h2>
+            <h2>
+              {props.enemyPokemonData.name}'s HP: {defenderHP}
+            </h2>
+            <button
+              onClick={() => {
+                const updatedAttacker = {
+                  ...props.userPokemonData,
+                  stats: [
+                    {
+                      ...props.userPokemonData.stats[0],
+                      base_stat: attackerHP,
+                    },
+                    ...props.userPokemonData.stats.slice(1),
+                  ],
+                };
+                const updatedDefender = {
+                  ...props.enemyPokemonData,
+                  stats: [
+                    {
+                      ...props.enemyPokemonData.stats[0],
+                      base_stat: defenderHP,
+                    },
+                    ...props.enemyPokemonData.stats.slice(1),
+                  ],
+                };
+                const result = fight(updatedAttacker, updatedDefender);
+                setAttackerHP(result.hpAttacker);
+                setDefenderHP(result.hpDefender);
+              }}
+            >
+              Fight!
+            </button>
+          </>
         </div>
-    
-    )
+      ) : (
+        <h2>Loading players</h2>
+      )}
+    </div>
+  );
 }
 
 export default BattleMenu;
