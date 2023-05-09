2bd789bd756b492ed7b375971046096745b80d0c
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2023-05-09 10:39:49 +0200

    added more fetches

--- pokegame/src/App.js
+++ pokegame/src/App.js
@@ -1,54 +1,59 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 
-const locationApi = "https://pokeapi.co/api/v2/location-area/";
+const locationApi = "https://pokeapi.co/api/v2/location/";
+const locationAreaApi = "https://pokeapi.co/api/v2/location-area/";
 const pokeApi = "https://pokeapi.co/api/v2/pokemon/";
 
 
 function App() {
   const [locationData, setLocationData] = useState([]);
+  const [locationAreaData, setLocationAreaData] = useState([]);
   const [pokemonData, setPokemonData] = useState();
 
   useEffect(() => {
     Promise.all(Array.from({length: 20}, (_, i) =>{
       console.log(i);
-      fetch(`https://pokeapi.co/api/v2/location-area/${i+1}`)
+      fetch(`${locationApi}${i+1}`)
       .then(res => res.json()
       .then((data)=>{
-        //console.log(data)
-        
+        //console.log(data.areas[0].url)
         setLocationData(oldData => [...oldData , data])
+        let areaObject = {
+          location: data.name,
+          id: data.id,
+          area: undefined
+        }
+        data.areas.length !==0 ?
+          fetch(data.areas[0].url).then(res => res.json()
+          .then((areaData)=>{
+            areaObject.area = areaData;
+            setLocationAreaData(oldData => [...oldData , areaObject])
+          })) : (
+            setLocationAreaData(oldData => [...oldData , areaObject])
+            )
       }))
     }))
   }, [])
-  locationData.sort((a,b) => a.id-b.id)
-  console.log(locationData);
-  
-  // useEffect(() => {
-  //   fetch(locationApi).then(response => response.json()).then(data=>{
-  //     //console.log(data)
-  //     setLocationData(data);
-  //   })
-  // }, [])
-
-  // useEffect(() => {
-  //   fetch(pokeApi).then(response => response.json()).then(data=>{
-  //     //console.log(data)
-  //     setPokemonData(data);
-  //   })
-  // }, [])
+  locationAreaData.sort((a,b) => a.id-b.id)
+  //console.log(locationData);
+  console.log(locationAreaData);
   
   return (
     <div className="App">
       {
-        locationData ? (
-          locationData.map((e, index)=>{
+        locationAreaData ? (
+          locationAreaData.map((e, index)=>{
+            let pokemon;
+            e.area !=undefined ?(
+             pokemon = e.area.pokemon_encounters[Math.floor(Math.random()*e.area.pokemon_encounters.length)].pokemon.name
+             ) : pokemon = "NO POKEMON HERE"
             return(
               <div key = {index}>
                 <h1>Area</h1>
-                <h2>{e.name}</h2>
+                <h2>{e.location}</h2>
                 <h1>Pokemon in Area</h1>
-                <h2>{e.pokemon_encounters[Math.floor(Math.random()*e.pokemon_encounters.length)].pokemon.name}</h2>
+                <h2>{pokemon}</h2>
               </div>
            )
           })
