44a930dae094f1d34e0e394a1c767f250b030426
L.Radi <radi.levente@gmail.com>
2023-05-10 14:07:59 +0200

    fix cards

--- pokegame/src/components/LocationCard.jsx
+++ pokegame/src/components/LocationCard.jsx
@@ -6,14 +6,16 @@ function Location(props) {
     const pokemonArray = props.pokemonArray;
     const pokemon = props.pokemon
     const handleClick = props.handleClick
+const pokemons = pokemonArray.join(", ").toUpperCase()
+    
 
   return (
-    <Card style={{ width: "18rem" }} bg="secondary">
+    <Card style={{ width: "18rem" }} bg="light">
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>Area: {location}</Card.Title>
-        <Card.Text>
-          Lórum ipse zatos grabárt horol: a törper készeg rösken, törgecskes enyzetség ez. A másik nevezgő pocki a konum surkoma, ez azonban általában a tetések hatós lertásától
+              <Card.Text>
+                <h5>In this area you may encounter: </h5>  {pokemons}
         </Card.Text>
         <Button variant="danger" onClick={() => handleClick(location, pokemon)}>Enter area!</Button>
       </Card.Body>
