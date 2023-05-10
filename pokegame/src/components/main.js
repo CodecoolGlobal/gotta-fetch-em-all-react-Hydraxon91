b9ce7bf7ae32dfef20d049b9aebf1de4c0ad48ad
L.Radi <radi.levente@gmail.com>
2023-05-10 11:46:46 +0200

    add Bootstrap

--- pokegame/src/components/main.js
+++ pokegame/src/components/main.js
@@ -1,37 +1,45 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
-function MainPage(props){
-    let locationAreaData = props.locationAreaData;
+function MainPage(props) {
+  let locationAreaData = props.locationAreaData;
 
-    function EnterBattle(location, pokemon){
-        console.log(`Entered battle in ${location} against ${pokemon}`);
-        
-        props.setEnemyPokemon(pokemon)
-        props.setBattleState(true)
-    }
+  function EnterBattle(location, pokemon) {
+    console.log(`Entered battle in ${location} against ${pokemon}`);
 
-    //console.log(locationAreaData)
+    props.setEnemyPokemon(pokemon);
+    props.setBattleState(true);
+  }
+
+  //console.log(locationAreaData)
+  return locationAreaData.map((e, index) => {
+    let pokemon;
+    e.area !== undefined
+      ? (pokemon =
+          e.area.pokemon_encounters[
+            Math.floor(Math.random() * e.area.pokemon_encounters.length)
+          ].pokemon.name)
+      : (pokemon = "NO POKEMON HERE");
     return (
-        
-                locationAreaData.map((e, index)=>{
-                    let pokemon;
-                  e.area !==undefined ?(
-                   pokemon = e.area.pokemon_encounters[Math.floor(Math.random()*e.area.pokemon_encounters.length)].pokemon.name
-                   ) : pokemon = "NO POKEMON HERE"
-                  return(
-                      <div key = {index}>
-                          <h1>Area</h1>
-                          <h2>{e.location}</h2>
-                          <h1>Pokemon in Area</h1>
-                          <h2>{pokemon}</h2>
-                          <button onClick={()=>EnterBattle(e.location, pokemon)}>Enter Area</button>
-                      </div>
-                 )
-                })  
-        
-        
-        // <h2>test</h2>
-    )
+      <div key={index}>
+        <h1>Area</h1>
+        <h2>{e.location}</h2>
+        <h1>Pokemon in Area</h1>
+        <h2>{pokemon}</h2>
+        {/* <button onClick={() => EnterBattle(e.location, pokemon)}>
+          Enter Area
+        </button> */}
+        <Button
+          variant="danger"
+          onClick={() => EnterBattle(e.location, pokemon)}
+        >
+          Enter area!
+        </Button>{" "}
+      </div>
+    );
+  });
+
+  // <h2>test</h2>
 }
 
 export default MainPage;
