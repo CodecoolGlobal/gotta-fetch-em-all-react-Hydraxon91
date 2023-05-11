484fd78e87ffee2cb97eb2c24dd2e67a44399406
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2023-05-11 13:10:12 +0200

    Updated pokemon data structure and made code work with it

--- pokegame/src/fight.js
+++ pokegame/src/fight.js
@@ -2,12 +2,12 @@ export default function fight(attacker, defender) {
   const min = 217;
   const max = 255;
   let winner = null;
-
-  let hpAttacker = attacker.stats[0].base_stat;
+  console.log(attacker);
+  let hpAttacker = attacker.currHP;
   let hpDefender = defender.stats[0].base_stat;
-  const BAttacker = attacker.stats[1].base_stat;
+  const BAttacker = attacker.data.stats[1].base_stat;
   const BDefender = defender.stats[1].base_stat;
-  const DAttacker = attacker.stats[2].base_stat;
+  const DAttacker = attacker.data.stats[2].base_stat;
   const DDefender = defender.stats[2].base_stat;
 
   const Z = Math.floor(Math.random() * (max - min + 1) + min);
@@ -24,12 +24,12 @@ export default function fight(attacker, defender) {
 
   hpDefender -= AD;
   hpAttacker -= DA;
-
+console.log(hpAttacker);
   // Update attacker's and defender's base_stat values in stats array
-  attacker.stats[0].base_stat = hpAttacker;
+  attacker.currHP = hpAttacker;
   defender.stats[0].base_stat = hpDefender;
-
-  if (hpAttacker > 0 && hpDefender <= 0) winner = attacker.name;
+  console.log(winner);
+  if (hpAttacker > 0 && hpDefender <= 0) winner = attacker.data.name;
   else if (hpDefender > 0 && hpAttacker <= 0) winner = defender.name;
   //else if (hpAttacker < 0 && hpDefender < 0) winner = "draw";
 
